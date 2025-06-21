5fed484959d130860bddbb918bbaeb5a7db25b81
lucasmiranda <lucasmsoares72@gmail.com>
2025-06-21 20:57:04 -0300

    removing Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import Navbar from "./sections/navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
-import Experiences from "./sections/Experiences";
-import Testimonial from "./sections/Testimonial";
 import Contact from "./sections/Contact";
 import Footer from './sections/Footer';
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
-      <Navbar />
+      <header>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#work">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
       <Hero />
       <About />
       <Projects />
